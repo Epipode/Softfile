368e28baa430ab2b625b02503dfc9863a27ddd99
Epipode nimbona <127045989+Epipode@users.noreply.github.com>
2024-08-15 09:54:33 +0200

    changes

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="rw">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <a href="">
+                <img src="sof.jpg" alt="Logo" />
+            </a>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="">Ahabanza</a></li>
+                <li><a href="">Injira</a></li>
+                <li><a href="">Iyandikishe</a></li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
